31e9eb06ec3cb7e244ecfa7543b38cf548e19a9e
crmin <crmin@superclass.io>
2023-01-28 19:17:37 +0900

    task 등록 시점에 broker가 죽은 경우에 대한 예외처리

--- works/views.py
+++ works/views.py
@@ -2,6 +2,8 @@ import random
 import time
 
 from django.http import HttpResponse
+from kombu.exceptions import OperationalError
+from loguru import logger
 
 from works.tasks import calculate_task
 
@@ -12,6 +14,11 @@ def process_work(request):
     tick = time.time()
     n = random.randrange(35, 42)
 
-    calculate_task.delay(n, tick)
+    try:
+        calculate_task.delay(n, tick)
+    except OperationalError:  # task 실행 시점에 이미 broker와 연결이 끊겨있음
+        # -> 유저가 실행을 요청할 때 이미 문제가 파악되므로 DB에 등록하지 않고 response로 실패를 보여줄 수 있음
+        logger.error('Cannot connect to broker')
+        return HttpResponse('Cannot connect to broker')
 
     return HttpResponse('done')
